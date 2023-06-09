a299374e985da4e1dd8e75d307b02df706380e31
lvsmsmvh <iamajavagod@gmail.com>
2023-06-10 02:54:46 +0300

    Migration from 1 to 2

--- app/src/main/java/com/cyberquick/hearthstonedecks/data/db/RoomDB.kt
+++ app/src/main/java/com/cyberquick/hearthstonedecks/data/db/RoomDB.kt
@@ -1,19 +1,23 @@
 package com.cyberquick.hearthstonedecks.data.db
 
+import androidx.room.AutoMigration
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.cyberquick.hearthstonedecks.data.db.dao.DeckDao
 import com.cyberquick.hearthstonedecks.data.db.entities.DeckEntity
 
 const val DATABASE_NAME = "room_local_db"
-private const val CURRENT_DB_VERSION = 2
 
 @Database(
+    version = 2,
     entities = [
         DeckEntity::class,
     ],
-    version = CURRENT_DB_VERSION
+    autoMigrations = [
+        AutoMigration(from = 1, to = 2)
+    ]
 )
+
 abstract class RoomDB : RoomDatabase() {
     abstract fun deckDao(): DeckDao
 }
